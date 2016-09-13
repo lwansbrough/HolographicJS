3dc92be6d124363b63eb37016779524c43d890d3
Lochlan Wansbrough <lochie@live.com>
2016-09-12 17:32:34 -0700

    Fixed dependencies

---
+++ HolographicJSTest/App.cpp
@@ -0,0 +1,163 @@
+﻿//
+// This file demonstrates how to initialize EGL in a Windows Store app, using ICoreWindow.
+//
+
+#include "pch.h"
+#include "app.h"
+#include "SimpleRenderer.h"
+
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Input;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Graphics::Holographic;
+using namespace Windows::Perception::Spatial;
+using namespace Microsoft::WRL;
+using namespace Platform;
+using namespace HolographicJS;
+
+using namespace HolographicJSTest;
+
+// Helper to convert a length in device-independent pixels (DIPs) to a length in physical pixels.
+inline float ConvertDipsToPixels(float dips, float dpi)
+{
+    static const float dipsPerInch = 96.0f;
+    return floor(dips * dpi / dipsPerInch + 0.5f); // Round to nearest integer.
+}
+
+// Implementation of the IFrameworkViewSource interface, necessary to run our app.
+ref class SimpleApplicationSource sealed : Windows::ApplicationModel::Core::IFrameworkViewSource
+{
+public:
+    virtual Windows::ApplicationModel::Core::IFrameworkView^ CreateView()
+    {
+        return ref new App();
+    }
+};
+
+// The main function creates an IFrameworkViewSource for our app, and runs the app.
+[Platform::MTAThread]
+int main(Platform::Array<Platform::String^>^)
+{
+    auto simpleApplicationSource = ref new SimpleApplicationSource();
+    CoreApplication::Run(simpleApplicationSource);
+    return 0;
+}
+
+App::App() :
+    mWindowClosed(false),
+    mWindowVisible(true),
+    mEglDisplay(EGL_NO_DISPLAY),
+    mEglContext(EGL_NO_CONTEXT),
+    mEglSurface(EGL_NO_SURFACE)
+{
+}
+
+// The first method called when the IFrameworkView is being created.
+void App::Initialize(CoreApplicationView^ applicationView)
+{
+    // Register event handlers for app lifecycle. This example includes Activated, so that we
+    // can make the CoreWindow active and start rendering on the window.
+    applicationView->Activated += 
+        ref new TypedEventHandler<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
+
+    // Logic for other event handlers could go here.
+    // Information about the Suspending and Resuming event handlers can be found here:
+    // http://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh994930.aspx
+}
+
+// Called when the CoreWindow object is created (or re-created).
+void App::SetWindow(CoreWindow^ window)
+{
+    window->VisibilityChanged +=
+        ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &App::OnVisibilityChanged);
+
+    window->Closed += 
+        ref new TypedEventHandler<CoreWindow^, CoreWindowEventArgs^>(this, &App::OnWindowClosed);
+
+    try
+    {
+        // Create a holographic space for the core window for the current view.
+        mHolographicSpace = HolographicSpace::CreateForCoreWindow(window);
+
+        // Get the default SpatialLocator.
+        SpatialLocator^ locator = SpatialLocator::GetDefault();
+
+        // Create a stationary frame of reference.
+        mStationaryReferenceFrame = locator->CreateStationaryFrameOfReferenceAtCurrentLocation();
+
+        // The HolographicSpace has been created, so EGL can be initialized in holographic mode.
+        //InitializeEGL(mHolographicSpace);
+		Host^ holographicJS = ref new Host(mHolographicSpace, mStationaryReferenceFrame);
+		try {
+			holographicJS->RunScript("app.js");
+		}
+		catch (Exception^ e) {
+			OutputDebugString(e->Message->Data());
+		}
+    }
+    catch (Platform::Exception^ ex)
+    {
+        if (ex->HResult == HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED))
+        {
+            // Device does not support holographic spaces. Initialize EGL to use the CoreWindow instead.
+        }
+    }
+}
+
+// Initializes scene resources
+void App::Load(Platform::String^ entryPoint)
+{
+    
+}
+
+
+// This method is called after the window becomes active.
+void App::Run()
+{
+    while (!mWindowClosed)
+    {
+        if (mWindowVisible)
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessAllIfPresent);
+
+            EGLint panelWidth = 0;
+            EGLint panelHeight = 0;
+            eglQuerySurface(mEglDisplay, mEglSurface, EGL_WIDTH, &panelWidth);
+            eglQuerySurface(mEglDisplay, mEglSurface, EGL_HEIGHT, &panelHeight);
+        }
+        else
+        {
+            CoreWindow::GetForCurrentThread()->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessOneAndAllPending);
+        }
+    }
+
+    // TODO: Clean up HolographicJS here
+}
+
+// Terminate events do not cause Uninitialize to be called. It will be called if your IFrameworkView
+// class is torn down while the app is in the foreground.
+void App::Uninitialize()
+{
+}
+
+// Application lifecycle event handler.
+void App::OnActivated(CoreApplicationView^ applicationView, IActivatedEventArgs^ args)
+{
+    // Run() won't start until the CoreWindow is activated.
+    CoreWindow::GetForCurrentThread()->Activate();
+}
+
+// Window event handlers.
+void App::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+    mWindowVisible = args->Visible;
+}
+
+void App::OnWindowClosed(CoreWindow^ sender, CoreWindowEventArgs^ args)
+{
+    mWindowClosed = true;
+}
---
+++ HolographicJSTest/app.js
@@ -0,0 +1,3 @@
+console.log('Attempting to create holographic rendering context...');
+const holographicContext = window.createCanvasRenderingContextHolographic();
+console.log('It worked!');
